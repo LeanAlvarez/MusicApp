f443cae77e4bcd7be00473cd15b9144a302fc95c
lean alvarez <leandro.alvarez8727@gmail.com>
2018-03-06 19:38:31 -0300

    fixes por la URL

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://leanalvarez.github.io/MusicApp/dist/logo.png')
     h1 MusicApp
     select(v-model="selectedCountry")
       option(v-for="country in countries" :value="country.value") {{ country.name }}
