a3ffa9cfe77e1210b997a10785cad1a20cd62c6e
lean alvarez <leandro.alvarez8727@gmail.com>
2018-03-06 19:10:34 -0300

    Initial commit

---
+++ src/API/config.js
@@ -0,0 +1,10 @@
+export default {
+  appname: 'platzimusic',
+  apiKey: 'a2d8dcb91c7f430032fa225373d51ddd',
+  secret: '2ab4cbbe5f3c3b5b079454205c00e334',
+  registeredTo: 'leanalvarez'
+}
+// Application namePlatzimusic
+// API keya2d8dcb91c7f430032fa225373d51ddd
+// Shared secret2ab4cbbe5f3c3b5b079454205c00e334
+// Registered toleanalvarez
